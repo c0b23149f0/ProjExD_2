3df97b47cfb065ccf89388fe760c2eb69987105f
Ryoma <c0b23149f0@edu.teu.ac.jp>
2024-10-01 14:53:49 +0900

    練習問題3完成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,7 +11,18 @@ DELTA={pg.K_UP:(0,-5),
        }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
+def check_bound(obj_rect: pg.Rect)->tuple[bool,bool]:
+    """
+    引数：こうかとんまたは爆弾のrect
+    戻り値：真理値タプル、横判定結果、縦判定結果
+    True：画面内、False：画面外
+    """
+    yoko,tate=True,True
+    if obj_rect.left<0 or WIDTH<obj_rect.right:
+        yoko=False
+    if obj_rect.top<0 or HEIGHT<obj_rect.bottom:
+        tate=False
+    return yoko,tate
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -40,14 +51,22 @@ def main():
             if key_lst[key]:
                 sum_mv[0]+=tpl[0]
                 sum_mv[1]+=tpl[1]
-        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        else:
+            kk_rct.move_ip(sum_mv)
+        yoko,tate =check_bound(bb_rct)
+        if not yoko:
+            vx*=-1
+        elif not tate:
+            vy*=-1
         bb_rct.move_ip(vx,vy)
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
+        #print(tmr)
 
 if __name__ == "__main__":
     pg.init()
